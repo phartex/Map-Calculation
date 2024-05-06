c979207240c200710d826045ccf4d752b44b6cbc
fateru tobi <fateruvictor96@gmail.com>
2024-05-06 23:25:35 +0100

    update

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -45,7 +45,7 @@ export class MapComponent implements OnInit {
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(this.mymap);
       let marker = L.marker(this.latLong).addTo(this.mymap);
 
