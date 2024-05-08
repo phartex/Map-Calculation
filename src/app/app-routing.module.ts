6fb86e682a4d2bd1337811a4e3d910a150601f4c
fateru tobi <fateruvictor96@gmail.com>
2024-05-08 19:40:16 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { mapPointGuard } from './guard/map-point.guard';
 
 const routes: Routes = [
   {
-    path: '', redirectTo: '/home', pathMatch: 'full'
+    path: '', redirectTo: 'home', pathMatch: 'full'
   },
   {
     path:'home',
